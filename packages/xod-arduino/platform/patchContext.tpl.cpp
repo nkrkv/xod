01c62291fe666a5bdc0af537dec52d5596a2edf9
Victor Nakoryakov <nail.xx@gmail.com>
2017-12-19 20:05:41 +0300

    optimize(xod-arduino): perform loop unroll [wip]
    start adjusting patch context (does not work at all)

--- packages/xod-arduino/platform/patchContext.tpl.cpp
+++ packages/xod-arduino/platform/patchContext.tpl.cpp
@@ -6,26 +6,52 @@ struct Storage {
   {{#each outputs}}
     {{ cppType type }} output_{{ pinKey }};
   {{/each}}
+
+  {{#each outputs}}
+    bool isOutputDirty_{{ pinKey }} : 1;
+  {{/each}}
 };
 
-struct Wiring {
-    EvalFuncPtr eval;
+enum Pins {
   {{#each inputs}}
-    UpstreamPinRef input_{{ pinKey }};
+    input_{{ pinKey }},
   {{/each}}
   {{#each outputs}}
-    const NodeId* output_{{ pinKey }};
+    input_{{ pinKey }},
+  {{/each}}
+    PIN_COUNT {{!-- A dummy to forget trailing comma problems --}}
+};
+
+struct Context {
+    Storage* storage;
+
+  {{#each inputs}}
+    {{ cppType type }}* _input_{{ pinKey }};
+  {{/each}}
+
+  {{#each inputs}}
+    bool _isInputDirty_{{ pinKey }};
   {{/each}}
 };
 
-State* getState(NodeId nid) {
-    return reinterpret_cast<State*>(getStoragePtr(nid, 0));
-}
-
 {{#each inputs}}
-using input_{{ pinKey }} = InputDescriptor<{{ cppType type }}, offsetof(Wiring, input_{{ pinKey }})>;
+template<> input_{{ pinKey }}::T getValue<input_{{ pinKey }}>(Context ctx) {
+    return *ctx._input_{{pinKey }};
+}
+{{/each}}
+{{#each outputs}}
+template<> output_{{ pinKey }}::T getValue<output_{{ pinKey }}>(Context ctx) {
+    return ctx.storage->otput_{{ pinKey }};
+}
 {{/each}}
 
 {{#each outputs}}
-using output_{{ pinKey }} = OutputDescriptor<{{ cppType type }}, offsetof(Wiring, output_{{ pinKey }}), offsetof(Storage, output_{{ pinKey }}), {{@index}}>;
+template<> emitValue<output_{{ pinKey }}>(Context ctx, output_{{ pinKey }}::T val) {
+    ctx.storage->otput_{{ pinKey }} = val;
+    ctx.storage->isOutputDirty_{{ pinKey }} = true;
+}
 {{/each}}
+
+State* getState(Context ctx) {
+    return &ctx.storage->state;
+}
